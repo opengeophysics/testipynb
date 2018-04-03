4952405467cc4b94704c8cf0f415ed134daec2cc
Lindsey Heagy <lindseyheagy@gmail.com>
2018-04-03 15:00:44 -0700

    Bump version: 0.0.2 → 0.0.3

--- nbtest/__init__.py
+++ nbtest/__init__.py
@@ -1,7 +1,7 @@
 from .nbtest import TestNotebooks
 
 
-__version__ = '0.0.2'
+__version__ = '0.0.3'
 __author__ = 'Lindsey Heagy'
 __license__ = 'MIT'
 __copyright__ = 'Lindsey Heagy'
--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ with open('README.rst') as f:
 
 setup(
     name = 'nbtest',
-    version = '0.0.2',
+    version = '0.0.3',
     packages = find_packages(),
     install_requires = [
         'future',
