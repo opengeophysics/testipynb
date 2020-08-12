f8961685418959f2efc972640ca1eb6fb7a47391
Lindsey Heagy <lindseyheagy@gmail.com>
2020-08-12 16:45:19 -0600

    Bump version: 0.0.2 → 0.0.3

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ with open("README.rst") as f:
 
 setup(
     name="testipynb",
-    version="0.0.2",
+    version="0.0.3",
     packages=find_packages(),
     install_requires=[
         "jupyter",
